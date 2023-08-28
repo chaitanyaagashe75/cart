4f26af6059e63fbf248ea6167a89b9ad2ea83840
chaitanya agashe <chaitanyaagashe75@gmail.com>
2023-08-28 10:12:44 +0530

    commiting 2

--- src/main/java/com/example/cart/controller/CartController.java
+++ src/main/java/com/example/cart/controller/CartController.java
@@ -42,6 +42,6 @@ public class CartController {
            return cartService.getAllCarts();
         }
         // gitu
-     // changes made in remote master 
+     // changes made in local master
     }
 
