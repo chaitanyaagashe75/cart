64b653f807b697a94caebb298a4bdc52c86b2873
chaitanyaagashe75 <139640217+chaitanyaagashe75@users.noreply.github.com>
2023-08-28 10:30:53 +0530

    Update CartController.java

--- src/main/java/com/example/cart/controller/CartController.java
+++ src/main/java/com/example/cart/controller/CartController.java
@@ -43,6 +43,6 @@ public class CartController {
         }
         // gitu
      // changes made in remote master
-    //chanage made for checking the pushing code from local master to remote branch1
+    //chanage made for checking the pushing code from local master to remote branch1 but this is not possile 
     }
 
